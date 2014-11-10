33cd5290197dc60dd71634b27c398775b2c96b5c
Andres Erbsen <andres@krutt.org>
2014-11-10 01:35:42 -0500

    a BFS that does not run

--- graph-final.v
+++ graph-final.v
@@ -22,25 +22,104 @@ Definition edgeStarts (g:graph) : list node := map (@fst node (list node)) g.
 Definition GoodGraph g := NoDup (edgeStarts g).
 Definition parent_t := list (node * node).
 
-Definition edgesFrom (g:graph) (v:node) := find (fun a => if node_eq_dec (fst a) v then true else false) g.
+Fixpoint lookupEdgesAndRemove (g:graph) (v:node) {struct g} : option (adj * graph) :=
+  match g with
+  | nil => None
+  | x::g'' => if node_eq_dec (fst x) v
+      then Some (x, g'')
+      else match lookupEdgesAndRemove g'' v with
+      | None => None
+      | Some p => let (y, g') := p in Some (y, x::g')
+      end
+  end.
 
-Definition isNone {A:Type} (oa:option A) := match oa with None => true | _ => false end.
+Lemma remove_length' : forall g a v neighbors g',
+    lookupEdgesAndRemove g a = Some (v, neighbors, g') -> length g = S (length g').
+  induction g; intros; [inversion H|].
+  simpl in *. destruct (node_eq_dec (fst a) a0).
+  - injection H; intros; subst; clear H. trivial.
+  - case_eq (lookupEdgesAndRemove g a0); intros; rewrite H0 in *; [|inversion H].
+    destruct p.
+    injection H; intros; subst; clear H.
+    apply f_equal; simpl.
+    apply (IHg _ _ _ _ H0).
+Qed.
 
-Fixpoint filterSome {A:Type} (xs:list (option A)): list A :=
+Fixpoint firstForWhichSomeAndTail {A B:Type} (f:A->option B) (xs:list A) {struct xs} : option (B * list A) :=
   match xs with
-  | nil => nil
-  | ox::xs' => match ox with
-    | None => filterSome xs'
-    | Some x => x::filterSome xs'
-  end
-end.
-
-Fixpoint bfs (g:graph) (frontier : list node) (parent:parent_t) : parent_t. refine(
-    let frontierAdjs := filterSome (map (edgesFrom g) frontier) in
-    match frontierAdjs with nil => parent | adjv::frontierAdjs' =>
-        let (v, neighbours) := adjv in
-        let frontier' := map (@fst node (list node)) frontierAdjs ++ _ in
-        let parent' := _ ++ parent in
-        bfs g frontier' parent'
+  | nil => None
+  | ox::xs' => match (f ox) with
+    | None => firstForWhichSomeAndTail f xs'
+    | Some x => Some (x, xs')
     end
-).
+  end. 
+
+Lemma remove_length : forall g v neighbors frontier g' frontier',
+  firstForWhichSomeAndTail (lookupEdgesAndRemove g) frontier = Some (v, neighbors, g', frontier') ->
+  length g = S (length g').
+  intros; subst; simpl.
+  generalize dependent v. generalize dependent neighbors.
+  induction frontier; intros; inversion H.
+  case_eq (lookupEdgesAndRemove g a); intros.
+  - rewrite H0 in H1.
+    injection H1; clear H1; intros; subst.
+    clear IHfrontier H.
+    rewrite (remove_length' _ _ _ _ _ H0); auto.
+  - eapply IHfrontier; clear IHfrontier.
+    rewrite H0 in H1. rewrite H1. auto.
+Qed.
+
+Function bfs'''' (args : graph * list node * parent_t)
+    {measure (fun args => length (fst (fst (args))))} : parent_t := 
+  let (args', parent) := args in let (g, frontier) := args' in
+  match firstForWhichSomeAndTail (lookupEdgesAndRemove g) frontier
+      with None => parent | Some (v, neighbors, g', frontier') =>
+  let parent' := map (fun u => (u,v)) neighbors ++ parent in
+  bfs'''' (g', frontier', parent')
+end.
+  intros; subst; simpl.
+  rewrite (remove_length _ _ _ _ _ _ teq1); auto.
+Qed.
+Definition bfs' g frontier := bfs'''' (g, frontier, nil).
+
+Fixpoint bfs'' (len_g:nat) (g:graph) (frontier : list node) (parent:parent_t)
+    {struct len_g} : option parent_t := 
+  match firstForWhichSomeAndTail (lookupEdgesAndRemove g) frontier
+    with None => Some parent | Some (((v, neighbors), g'), frontier') =>
+  let parent' := map (fun u => (u,v)) neighbors ++ parent in
+  match len_g with 0 => None | S len_g' => bfs'' len_g' g' frontier' parent'
+end end.
+Definition bfs''' g frontier := bfs'' (length g) g frontier nil.
+
+Ltac neqConstructor := simpl; unfold not; intro H_; inversion H_.
+
+Lemma bfs_terminates : forall g frontier, bfs''' g frontier <> None.
+  unfold bfs'''.
+  intros g frontier.
+  remember ([]) as parent; clear Heqparent.
+  remember (length g) as l.
+  generalize Heql; clear Heql.
+  generalize dependent parent.
+  generalize dependent frontier.
+  generalize dependent g.
+  induction l; intros; subst.
+    Focus 1. destruct g; [|inversion Heql]. simpl. induction frontier; neqConstructor. auto.
+  simpl.
+  case_eq (firstForWhichSomeAndTail (lookupEdgesAndRemove g) frontier); intros; [|neqConstructor].
+  destruct p; destruct p; destruct a.
+  eapply IHl.
+  specialize (remove_length _ _ _ _ _ _ H); intro H'.
+  rewrite <- Heql in H'.
+  auto.
+Qed.
+
+Definition bfs (g:graph) (frontier:list node) : parent_t.
+  remember (bfs''' g frontier) as ret.
+  destruct ret.
+  - apply p.
+  - destruct (bfs_terminates _ _ (eq_sym Heqret)).
+Qed.
+
+Example ex1 : (bfs [(Node 0,[Node 1])] [Node 0]) = [(Node 1, Node 0)].
+  compute. (* Why does it not work? *)
+Abort.
